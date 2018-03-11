0f96b4266b7b3dcbb58bd3396f29120f7981d16c
David Ly <d.ly@mail.utoronto.ca>
2018-03-11 08:22:45 -0400

    Completed Task 4.

--- align_ibm1.py
+++ align_ibm1.py
@@ -48,11 +48,15 @@ def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
     AM[SENTEND][SENTEND] = 1
 
     # Iterate between E and M steps
-
-    
+    #for i in range(len(aligned_sentences[0])):
+    eng_sentences = aligned_sentences[0]
+    fre_sentences = aligned_sentences[1]
+    for i in range(max_iter):
+        em_step(AM, eng_sentences, fre_sentences)  # pass AM to function and have it modify it directly!
 
     return AM
     
+
 # ------------ Support functions --------------
 def read_hansard(train_dir, num_sentences):
     """
@@ -132,11 +136,8 @@ def initialize(aligned_sentences):
     eng_word_to_occurence_with_fre_words = {}
 
     for i in range(len(eng_sentences)):
-        eng_tokens = eng_sentences[i].split()
-        eng_tokens = eng_tokens[1:-1]  # Remove SENTSTART/SENTEND
-
-        fre_tokens = fre_sentences[i].split()
-        fre_tokens = fre_tokens[1:-1]
+        eng_tokens = tokenize_and_clean(eng_sentences[i])  # Remove SENTSTART/SENTEND
+        fre_tokens = tokenize_and_clean(fre_sentences[i])
 
         # for each English token, if it's not in our dict, add it with an empty set as its value
         # union to its set a set of all French tokens in this sentence
@@ -153,11 +154,69 @@ def initialize(aligned_sentences):
             initialized_AM[eng_word][fre_word] = 1.0 / num_words_occurences
 
     return initialized_AM
-	
+
+
+def tokenize_and_clean(s):
+    """
+    Tokenize and remove SENTSTART and SENTEND.
+    """
+    tokens = s.split()
+    return tokens[1:-1]
+
     
 def em_step(t, eng, fre):
     """
 	One step in the EM algorithm.
 	Follows the pseudo-code given in the tutorial slides.
 	"""
-	# TODO
+
+
+    # t refers to model parameters, contents of AM
+    # In this sense, AM is essentially the t distribution from class, e.g.,
+    # >> AM[‘bird’][‘oiseau’] = 0.8   % t(oiseau|bird) = 0.8
+
+    # since we are running the EM algorithm 5-25 times, what is the thing that changes between iterations?
+    # from the tut slides, each iter seems to narrow down on alignment of eng to fre words...
+    # looks like we can modify the AM itself in the Maximize step
+    # which we then use in each iteration for the Expectation step
+
+    # Initialization already handled outside this function
+
+    #eng_words = tokenize_and_clean(eng)
+    #fre_words = tokenize_and_clean(fre)
+
+    t_count = {}
+    total = {}
+
+    # for each sentence pairs
+    for i in range(len(eng)):
+        eng_sentence = eng[i]
+        fre_sentence = fre[i]
+        eng_words = tokenize_and_clean(eng_sentence)
+        fre_words = tokenize_and_clean(fre_sentence)
+
+        # Expectation step
+        for eng_word in set(eng_words):
+            denom_c = 0
+            for fre_word in set(fre_words):
+                denom_c += t[eng_word][fre_word] * eng_words.count(eng_word)
+
+            if eng_word not in t_count:
+                t_count[eng_word] = {}
+            for fre_word in set(fre_words):
+                if fre_word not in t_count[eng_word]:
+                    t_count[eng_word][fre_word] = 0
+                t_count[eng_word][fre_word] += (t[eng_word][fre_word] * eng_words.count(eng_word) * fre_words.count(fre_word)) / denom_c
+
+                if fre_word not in total:
+                    total[fre_word] = 0
+                total[fre_word] += (t[eng_word][fre_word] * eng_words.count(eng_word) * fre_words.count(fre_word)) / denom_c
+
+        #print(t_count)
+        #print(total)
+
+    # Maximization step
+    for fre_word in total:
+        for eng_word in t_count:
+            if fre_word in t_count[eng_word]:
+                t[eng_word][fre_word] = t_count[eng_word][fre_word] / total[fre_word]
--- test_align_ibm1.py
+++ test_align_ibm1.py
@@ -4,7 +4,7 @@ from align_ibm1 import *
 train_dir = "A2_SMT/data/Hansard/Training/"
 output_path = "align"  # will become align.pickle, and saved in this directory
 num_sentences = 100
-max_iter = 25
+max_iter = 15  # range from 5-25
 
 def main():
 
